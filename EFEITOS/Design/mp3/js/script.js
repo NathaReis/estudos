e16355bd9e072d3629d70b99e2829047429867d0
NathaReis <nathan.david@siriusgroup.com.br>
2024-01-04 21:45:36 -0300

    Bonito

--- EFEITOS/Design/mp3/js/script.js
+++ EFEITOS/Design/mp3/js/script.js
@@ -1,4 +1,5 @@
-const $audio = document.querySelector("audio")
+const $audio = document.querySelector("#musica")
+const $click = document.querySelector("#efeito")
 const $title = document.querySelector(".title")
 const $img = document.querySelector(".img")
 const $recado = document.querySelector(".recado")
@@ -11,6 +12,7 @@ const $random = document.querySelector("#random")
 const $reload = document.querySelector("#reload")
 const $perfil = document.querySelector(".perfil")
 let intervalo;
+$click.volume = .2
 
 const config = {
     vol: .5,
@@ -24,19 +26,19 @@ const musics = [
         nome: 'Abismo',
         recados: 'ESPERTA',
         src: './music/abismo.mp3',
-        img: './img/perfil_t.jpg'
+        img: './img/p_um.jpg'
     },
     {
         nome: 'De Olhos Abertos',
         recados: 'LINDA',
         src: './music/de_olhos_abertos.mp3',
-        img: './img/perfil_t.jpg'
+        img: './img/p_dois.jpg'
     },
     {
         nome: 'Meu Bem Querer',
         recados: 'MEIGA',
         src: './music/meu_bem_querer.mp3',
-        img: './img/perfil_t.jpg'
+        img: './img/p_tres.jpg'
     },
 ]
 
@@ -49,7 +51,7 @@ const f_selected_music = () =>
         //Validar se a música já foi usada
         while(config.block_list.includes(pos_ran))
         {
-            if(cont > musics.length)
+            if(cont >= musics.length)
             {
                 config.block_list = []
             }
@@ -94,6 +96,7 @@ const f_ran = () =>
     {
         config.ran = false
     }//Se existir a classe random
+    $click.play()
 }//Random
 $random.onclick = () => f_ran();
 
@@ -122,6 +125,7 @@ $pause.onclick = () => f_pause();
 const f_reload = () =>
 {
     $audio.currentTime = 0
+    $click.play()
 }//Reload
 $reload.onclick = () => f_reload();
 
@@ -159,6 +163,7 @@ $last.onclick = () => f_last();
 
 const f_selected = (button) =>
 {
+    $click.play()
     if(button.id == 'play' || button.id == 'pause')
     {
         if(button.id == 'play')
